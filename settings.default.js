38034f170e4b272c2d501ce7922d24c52526b5aa
MGF1998 <nikithekid@hotmail.de>
2017-01-08 14:50:08 +0100

    updated default settings

--- settings.default.js
+++ settings.default.js
@@ -1,5 +1,6 @@
 module.exports = {
     token: "[your bot token]",
     owner_id: "[your user id]",
-    prefix: "[your desired prefix]"
+    prefix: "[your desired prefix]",
+    default_game: "[your desired game]"
 };
