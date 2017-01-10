501a1c113aa0d8f483f7c86a65df84486e16a677
TheBinaryBee <coolicooli1@hotmail.de>
2017-01-10 14:25:28 +0100

    readded missing files

--- data/userlist.js
+++ data/userlist.js
@@ -1,6 +1,20 @@
 //This list is only effective when useDiscordRoles is false.
 //This also requires manually reloading the bot. Using roles is more convenient.
 module.exports = {
-    banned : [],
-    access : []
-}
+    "banned": {
+        "66980998011420672": {
+            "expires": 1484003745286,
+            "id": 66980998011420670
+        }
+    },
+    "mods": {
+        "178470784984023040": {
+            "access": 99,
+            "id": 178470784984023040
+        },
+        "128985967875850240": {
+            "access": 99,
+            "id": 128985967875850240
+        }
+    }
+};
