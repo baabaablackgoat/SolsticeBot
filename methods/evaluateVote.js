f454f0655e112657222bbef0d67b890111cd99ac
TheBinaryBee <coolicooli1@hotmail.de>
2017-01-11 22:12:08 +0100

    fixed voting evaluation

--- methods/evaluateVote.js
+++ methods/evaluateVote.js
@@ -1,14 +1,16 @@
 //checks if vote is now passed
-module.exports = function(bot, msg, voteAction) {
+module.exports = function (bot, msg, voteAction) {
     const refreshUser = require("./refreshUser");
-    refreshUser(bot);
-    var all = user.length;
-    var voted = 0;
+    const user = refreshUser(bot);
+    let all = user.length;
+    let voted = 0;
+
     user.forEach(function (currentUser) {
         if (bot._dispatcher.votes[voteAction].indexOf(currentUser.id) > -1) {
             voted += 1;
         }
     });
     msg.channel.sendMessage(voteAction + ": " + voted + " / " + all);
+
     return ((voted / all) >= 0.5);
 };
