56c3699e0450b3cc81568f76175351e052c042dc
MGF1998 <nikithekid@hotmail.de>
2017-01-12 17:24:22 +0100

    giving @FelixRilling a boner for outsourcing

---
+++ data/memelist.js
@@ -0,0 +1,44 @@
+module.exports = {
+    defaultmemes: {
+            prefixes: [],
+            values: [
+                "https://www.reddit.com/r/kreiswichs/",
+                "https://www.reddit.com/r/nottheonion/",
+                "https://www.reddit.com/r/showerthoughts/",
+                "https://www.reddit.com/r/GlobalOffensive/",
+                "https://www.reddit.com/r/Overwatch/",
+                "https://www.reddit.com/r/iamverysmart/",
+                "https://www.youtube.com/watch?v=yXXyfeWJz1M",
+                "https://www.youtube.com/watch?v=zXSWb1tMmZY",
+                "https://www.youtube.com/watch?v=rR5Wh7CR8L8",
+                "https://www.youtube.com/watch?v=bLE7zsJk4AI",
+                "https://www.youtube.com/watch?v=FnzSGSDhbqg",
+                "https://www.youtube.com/watch?v=MdnWUX-zqao",
+                "https://www.youtube.com/watch?v=D8YZkSf8rok",
+                "https://www.youtube.com/watch?v=4naqDA01Q_c",
+                "https://www.youtube.com/watch?v=AS669AzO8bw",
+                "https://www.youtube.com/watch?v=rFiXp-dBfMY",
+                "https://www.youtube.com/watch?v=1QaeZRG4k6w",
+            ]
+        },
+        wan1: {
+            prefixes: ["wearenumberone","we are number one","number one", "numberone"],
+            values: [
+                "https://www.youtube.com/watch?v=xILWtD_Q23A",
+                "https://www.youtube.com/watch?v=cVKQpr1pPjM",
+                "https://www.youtube.com/watch?v=U1mQHtUjLWI",
+                "https://www.youtube.com/watch?v=ORpQlDuIUs0",
+                "https://www.youtube.com/watch?v=pGw0HjGWKQY",
+                "https://www.youtube.com/watch?v=cdi-CMwSXlQ"
+            ]
+        },
+        maimais: {
+            prefixes: ["maimais","maimai","deutsch","german"],
+            values: [
+            "https://www.youtube.com/watch?v=_rk_HiqvZcA",
+            "https://www.youtube.com/watch?v=nwAL06N3XX4",
+            "https://www.youtube.com/watch?v=6_4fhTO4-hg",
+            "https://www.youtube.com/watch?v=1mvAecXHFaQ",
+            ]
+        }
+};
