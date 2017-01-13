137a766448a47c35dbe1b4d23657355c0e884fa9
TheBinaryBee <coolicooli1@hotmail.de>
2017-01-13 20:44:25 +0100

    fixed userlist parsing

--- methods/accessCheck.js
+++ methods/accessCheck.js
@@ -1,6 +1,7 @@
 //Checks if the user who called a command that requires special access has the permissions to do so. Returns true if okay. 
 module.exports = function(msg, requiredAccess, punishment) {
     const userlist = require("./../data/userlist.json");
+    
     if (!userlist.mods.hasOwnProperty(msg.author.id) || userlist.mods[msg.author.id].access < requiredAccess) {
         if (punishment) { //...if not false (bool) to allow passing the punishment directly
             const applyBotBan = require("./applyBotBan");
--- methods/giveAccess.js
+++ methods/giveAccess.js
@@ -1,11 +1,12 @@
-module.exports = function(userid,access,bot,msg,args,options) {
+module.exports = function (userid, access, bot, msg, args, options) {
+    console.log(userid, access);
     const fs = require("fs");
-    let userlist = JSON.parse(fs.readFileSync('./data/userlist.json', 'utf8'));
+    let userlist = require('../data/userlist.json');
     if (!userlist.mods.hasOwnProperty(userid)) {
         userlist.mods[userid] = {
             "id": Number(userid)
         };
     }
     userlist.mods[userid].access = access;
-    fs.writeFile('./data/userlist.json', JSON.stringify(userlist,"  ","  "));
+    fs.writeFile('./data/userlist.json', JSON.stringify(userlist, "  ", "  "));
 };
