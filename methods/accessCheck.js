c79f6c570d76ad949cef0cae1c7243d398db42e2
MGF1998 <nikithekid@hotmail.de>
2017-01-13 21:27:23 +0100

    fixing the asynchronous bug thingy

--- methods/accessCheck.js
+++ methods/accessCheck.js
@@ -11,7 +11,7 @@ module.exports = function (msg, requiredAccess, punishment) {
         if (punishment) {
             applyBotBan(msg, "<@!" + msg.author.id + ">", punishment);
         }
-        msg.channel.sendMessage(msg.author.name+", you don't have access to this command. ("+userAccess+"<"+requiredAccess+")");
+        msg.channel.sendMessage(msg.author.username+", you don't have access to this command. ("+userAccess+"<"+requiredAccess+")");
         return false;
     }
 };
--- methods/giveAccess.js
+++ methods/giveAccess.js
@@ -2,12 +2,11 @@ const fs = require("fs");
 const userlist = require('../data/userlist.json');
 
 module.exports = function (userid, access, bot, msg, args, options) {
-    console.log(userid, access);
     if (!userlist.mods.hasOwnProperty(userid)) {
         userlist.mods[userid] = {
             "id": Number(userid)
         };
     }
     userlist.mods[userid].access = access;
-    fs.writeFile('./data/userlist.json', JSON.stringify(userlist, "  ", "  "));
+    fs.writeFileSync('./data/userlist.json', JSON.stringify(userlist, "  ", "  "));
 };
