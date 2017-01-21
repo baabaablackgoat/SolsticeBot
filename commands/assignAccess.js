6b4056892148cbee5b7bf5a5e9ef878c0c11bfa9
TheBinaryBee <coolicooli1@hotmail.de>
2017-01-21 22:46:41 +0100

    updated arg offset

--- commands/assignAccess.js
+++ commands/assignAccess.js
@@ -2,12 +2,15 @@ const getIDfromMention = require("./../methods/getIDfromMention");
 const giveAccess = require("./../methods/giveAccess");
 
 module.exports = function (bot, msg, args, options) {
-    if (getIDfromMention(msg)) {
-        const level = Number(args[1]);
+    const mentionIds = getIDfromMention(msg);
+
+    if (mentionIds) {
+        const argOffset = mentionIds.length;
+        const level = Number(args[argOffset]);
 
         if (!isNaN(level)) {
-            giveAccess(getIDfromMention(msg), level, bot, msg, args, options);
-            msg.channel.sendMessage("Assigned access " + level + " to the user with the ID " + getIDfromMention(msg));
+            giveAccess(mentionIds, level, bot, msg, args, options);
+            msg.channel.sendMessage("Assigned access " + level + " to the user with the ID " + mentionIds);
         } else {
             msg.channel.sendMessage("You didn't provide a valid access level.");
         }
