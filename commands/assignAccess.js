480e31e0007be3cf65c5002370fed207c0259e85
TheBinaryBee <coolicooli1@hotmail.de>
2017-01-21 22:34:17 +0100

    fixed access level parse

--- commands/assignAccess.js
+++ commands/assignAccess.js
@@ -2,14 +2,12 @@ const getIDfromMention = require("./../methods/getIDfromMention");
 const giveAccess = require("./../methods/giveAccess");
 
 module.exports = function (bot, msg, args, options) {
-    console.log(msg);
-
-console.log(getIDfromMention(msg));
-
     if (getIDfromMention(msg)) {
-        if (typeof args[1] === "number") {
-            giveAccess(getIDfromMention(msg), args[1], bot, msg, args, options);
-            msg.channel.sendMessage("Assigned access " + args[1] + " to the user with the ID " + getIDfromMention(msg));
+        const level = Number(args[1]);
+
+        if (level) {
+            giveAccess(getIDfromMention(msg), level, bot, msg, args, options);
+            msg.channel.sendMessage("Assigned access " + level + " to the user with the ID " + getIDfromMention(msg));
         } else {
             msg.channel.sendMessage("You didn't provide a valid access level.");
         }
