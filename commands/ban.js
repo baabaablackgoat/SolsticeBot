1b2fdfe39d605d61d680a573e70cbc4e26df1e31
MGF1998 <nikithekid@hotmail.de>
2017-01-22 20:29:52 +0100

    Checking for permissions in ban and kick

--- commands/ban.js
+++ commands/ban.js
@@ -1,9 +1,17 @@
 const getMembersfromMention = require("./../methods/getMemberfromMention");
 module.exports = function(bot,msg,args,options) {
+    if (!msg.member.hasPermission("BAN_MEMBERS")) {
+        msg.channel.sendMessage("You don't have the Ban Members permission.");
+        return;
+    }
     let members = getMembersfromMention(msg);
     for (let i = 0; i < members.id_list.length; i++) {
+        if (members.values[members.id_list[i]].highestRole.position >= msg.member.highestRole.position){
+            msg.channel.sendMessage("You cannot ban this user! He is on the same or a higher role!");
+            return;
+        }
         members.values[members.id_list[i]].ban()
-            .then(() => {msg.channel.sendMessage("Goodbye forever.");})
+            .then(() => {msg.channel.sendMessage("Goodbye forever. https://www.youtube.com/watch?v=FXPKJUE86d0");})
             .catch(err => {
                 msg.channel.sendMessage("Something went wrong... Maybe I do not have the permissions to ban this user.");
                 console.log(err);
--- commands/kick.js
+++ commands/kick.js
@@ -1,7 +1,15 @@
 const getMembersfromMention = require("./../methods/getMemberfromMention");
 module.exports = function(bot,msg,args,options) {
+    if (!msg.member.hasPermission("KICK_MEMBERS")) {
+        msg.channel.sendMessage("You don't have the Kick Members permission.");
+        return;
+    }
     let members = getMembersfromMention(msg);
     for (let i = 0; i < members.id_list.length; i++) {
+        if (members.values[members.id_list[i]].highestRole.position >= msg.member.highestRole.position){
+            msg.channel.sendMessage("You cannot kick this user! He is on the same or a higher role!");
+            return;
+        }
         members.values[members.id_list[i]].kick()
             .then(() => {msg.channel.sendMessage("Time tooooo say goooodbyyyyyeeee...");})
             .catch(err => {
