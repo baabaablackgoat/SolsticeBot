45bb84a037170942d4617944bacaa5d4142d71d8
MGF1998 <nikithekid@hotmail.de>
2017-02-06 12:38:44 +0100

    using try-catch to throw errors

--- commands/eval.js
+++ commands/eval.js
@@ -1,3 +1,8 @@
 module.exports = function(bot,msg,args,options) {
-    msg.channel.sendCode("fix",eval(args.join(" ")));
+    try {
+        msg.channel.sendCode("fix",eval(args.join(" ")));
+    }
+    catch(err) {
+        msg.channel.sendCode("fix","-ERROR-\n"+err);
+    }  
 };
