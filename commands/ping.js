8bc0447aff57b6352fdf14d3c4c82a32a3b342ff
MGF1998 <nikithekid@hotmail.de>
2017-02-10 16:27:37 +0100

    initial ping update

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,12 @@
+const Discord = require("discord.js");
 module.exports = function (bot, msg, args, options) {
-    msg.channel.sendMessage("Pong!");
+    let embed = new Discord.RichEmbed();
+    let temp = Math.floor(bot.uptime/1000);
+    let uptime = Math.floor(temp/86400)+"d"+Math.floor((temp%86400)/3600)+"h"+Math.floor((temp%3600)/60)+"m"+temp%60+"s";
+    temp = bot.readyAt;
+    let lastReady = temp.getFullYear()+"-"+(temp.getMonth()+1)+"-"+temp.getDate()+" "+temp.getHours()+":"+temp.getMinutes()+":"+temp.getSeconds();
+    embed.setColor([0,125,255]);
+    embed.setTitle("Ping: "+ Math.ceil(bot.ping));
+    embed.setDescription("Uptime: "+uptime+" | "+lastReady);
+    msg.channel.sendEmbed(embed);
 };
